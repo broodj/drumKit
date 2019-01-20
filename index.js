d0dcaed059c76c72e141b0191b023950d7e44705
Brody <brodywilton@hotmail.com>
2019-01-20 21:56:53 +0000

    Add keyboard event listeners, re-order code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let numberOfDrumButtons = document.querySelectorAll('.drum').length;
-
 //Audio sounds to be played
 var crash = new Audio('sounds/crash.mp3');
 var kickBass = new Audio('sounds/kick-bass.mp3');
@@ -9,14 +7,54 @@ var tomTwo = new Audio('sounds/tom-2.mp3');
 var tomThree = new Audio('sounds/tom-3.mp3');
 var tomFour = new Audio('sounds/tom-4.mp3');
 
-function drumsInTheDeep(){
-  var drums = document.querySelectorAll('.drum');
+var drums = document.querySelectorAll('.drum');
+var numberOfDrumButtons = document.querySelectorAll('.drum').length;
 
+//add event listeners to all drum buttons for mouseclicks
+function drumsInTheDeep(){
   for (i = 0; i < numberOfDrumButtons; i++ ){
     drums[i].addEventListener('click', drumClicked);
   }
 }
 
+//initiate click event listeners
+drumsInTheDeep();
+
+//initiate and create keyboard event listeners
+document.addEventListener('keyup', function (event) {
+  switch(event.keyCode) {
+      case 87:
+          // w key pressed
+          crash.play();
+          break;
+      case 65:
+          // a key pressed
+          kickBass.play();
+          break;
+      case 83:
+          // s key pressed
+          snare.play();
+          break;
+      case 68:
+          // d key pressed
+          tomOne.play();
+          break;
+      case 74:
+          // j key pressed
+          tomTwo.play();
+          break;
+      case 75:
+          //k key pressed
+          tomThree.play();
+          break;
+      case 76:
+          //l key pressed
+          tomFour.play();
+          break;
+  }
+});
+
+//When drum button is clicked, relevant sound plays
 function drumClicked(){
   var classList = this.classList;
 
@@ -53,6 +91,3 @@ function drumClicked(){
       alert('Incorrect drum click, try again');
   }
 }
-
-
-drumsInTheDeep();
